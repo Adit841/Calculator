a70393c0c4897a46fcb5643d94f4905f4dbde99e
Aditya Gour <adityagour0841@gmail.com>
2026-03-04 22:45:05 +0530

    adding keyboard functionality

--- script.js
+++ script.js
@@ -13,6 +13,12 @@ let previous;
 
 numbers.forEach((button) => {
   button.addEventListener("click", () => {
+    if (
+      button.textContent === "." &&
+      currentDisplay.textContent.includes(".")
+    ) {
+      return;
+    }
     currentDisplay.textContent += button.textContent;
   });
 });
@@ -24,13 +30,13 @@ operators.forEach((button) => {
 
     previousDisplay.textContent = previous + " " + operator;
     currentDisplay.textContent = "";
-    if(currentDisplay.textContent === ""){
+    if (currentDisplay.textContent === "") {
       return;
     }
   });
 });
 
-function calculate(){
+function calculate() {
   let current = currentDisplay.textContent;
 
   let a = parseFloat(previous);
@@ -38,23 +44,76 @@ function calculate(){
 
   let result;
 
-  if(operator === "+"){
+  if (operator === "+") {
     result = a + b;
   }
 
-  if(operator === "-"){
+  if (operator === "-") {
     result = a - b;
   }
 
-  if(operator === "X" ){
+  if (operator === "X") {
     result = a * b;
   }
 
-  if(operator === "/" ){
+  if (operator === "/") {
     result = a / b;
   }
 
   currentDisplay.textContent = result;
   previousDisplay.textContent = "";
 }
-equal.addEventListener('click', calculate);
+equal.addEventListener("click", calculate);
+
+reset.addEventListener("click", () => {
+  previous = "";
+  operator = "";
+
+  previousDisplay.textContent = "";
+  currentDisplay.textContent = "";
+});
+
+clear.addEventListener("click", () => {
+  currentDisplay.textContent = currentDisplay.textContent.slice(0, -1);
+});
+
+percent.addEventListener("click", () => {
+  let current = parseFloat(currentDisplay.textContent);
+
+  if (!isNaN(current)) {
+    currentDisplay.textContent = current / 100;
+  }
+});
+
+document.addEventListener('keydown', (e) => {
+
+  let key = e.key;
+
+  if(!isNaN(key) || key === "."){
+    currentDisplay.textContent +=key;
+  }
+
+  if(key === "+" || key === "-" || key === "X" || key === "/" ){
+    operator = key;
+    previous = currentDisplay.textContent;
+
+    previousDisplay.textContent = previous + " " + operator;
+    currentDisplay.textContent = "";
+  }
+
+  if(key === "Enter"){
+    calculate();
+  }
+
+  if(key === "Backspace"){
+    currentDisplay.textContent = currentDisplay.textContent.slice(0, -1);
+  }
+
+  if(key === "Escape"){
+    previous = "";
+    operator = "";
+
+    previousDisplay.textContent = "";
+    currentDisplay.textContent = "";
+  }
+})
